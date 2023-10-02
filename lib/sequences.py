6c416c0289db1225ced5f2d38c071665efffff12
shawnconway <shawnway210@gmail.com>
2023-10-02 12:02:18 -0400

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci = []
+    if length >= 1:
+        fibonacci.append(0)
+    if length >= 2:
+        fibonacci.append(1)
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    print(fibonacci)
     pass
